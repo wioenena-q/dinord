d5634ca1dff517979c7d898f67b24ad004d2ea31
wioenena-q <brs_kprl09@outlook.com>
2021-06-17 18:47:07 +0300

    Everything is reset.

--- deps.ts
+++
@@ -1,4 +0,0 @@
-export { Tiger } from "https://deno.land/x/tiger@1.0.4/mod.ts";
-export { EventEmitter } from "https://deno.land/std@0.97.0/node/events.ts";
-export { config } from "https://deno.land/x/dotenv/mod.ts";
-export { Collection } from "https://deno.land/x/dinordcollection@1.0.2/mod.ts";
