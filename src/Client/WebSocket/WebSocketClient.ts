6cde26d97ca590e0217a02e0f535b92d972dcdc6
wioenena-q <brs_kprl09@outlook.com>
2021-06-18 18:49:09 +0300

    A simple link has been created.

--- src/Client/WebSocket/WebSocketClient.ts
+++ src/Client/WebSocket/WebSocketClient.ts
@@ -1,3 +1,53 @@
+import { EventEmitter } from "../../../deps.ts";
+import * as Constants from "../../Utils/Constants.ts";
 
+function connectWebSocket(url:string):Promise<WebSocket> {
+    return new Promise<WebSocket>((res, rej) => {
+        const socket:WebSocket = new WebSocket(url);
 
+        socket.onopen = () => {
+            res(socket);
+        };
+        socket.onerror = rej;
+    });
+}
 
+/**
+ *
+ * The class to connect to the API.
+ * @export
+ * @class WebSocketClient
+ * @extends {EventEmitter}
+ */
+export class WebSocketClient extends EventEmitter {
+    /**
+     *
+     * The WebSocket class.
+     * @private
+     * @type {WebSocket}
+     * @memberof WebSocketClient
+     */
+    private socket!:WebSocket;
+
+    /**
+     *
+     * The total guild size.
+     * @private
+     * @type {number}
+     * @memberof WebSocketClient
+     */
+    private guildSize!:number;
+
+    /**
+     *
+     * The method to connect to the API.
+     * @memberof WebSocketClient
+     */
+    public async connect() {
+        this.socket = await connectWebSocket(Constants.Discord.GATEWAY);
+        this.socket.onmessage = this.onMessage;
+    }
+
+    private onMessage(d:unknown) {
+    }
+}
