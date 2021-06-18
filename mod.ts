947ee7e5936dca349d8e3a7460e91f69c83f3e4c
wioenena-q <brs_kprl09@outlook.com>
2021-06-19 01:16:42 +0300

    Other structures have been exported. (on mod.ts)

--- mod.ts
+++ mod.ts
@@ -1,1 +1,5 @@
 export * from "./src/Client/Client.ts";
+export * from "./src/Client/WebSocket/WebSocketClient.ts";
+export * from "./src/Types/IClientTypes.ts";
+export * from "./src/Types/IPayload.ts";
+export * from "./src/Utils/Constants.ts";
