0e70102466299cf1419ea91ca4f5a4787deee3f7
wioenena-q <brs_kprl09@outlook.com>
2021-06-18 19:27:42 +0300

    The main 'Client' class has been created and necessary actions have been taken to make the ws connection.

---
+++ src/Client/Client.ts
@@ -0,0 +1,67 @@
+import { EventEmitter } from "../../deps.ts";
+import { ClientOptions } from "../Types/ClientTypes.ts";
+import { WebSocketClient } from "./WebSocket/WebSocketClient.ts";
+
+export class Client extends EventEmitter {
+    /**
+     *
+     * The options for client.
+     * @private
+     * @type {ClientOptions}
+     * @memberof Client
+     */
+    private options: ClientOptions;
+
+    /**
+     *
+     * The class to connect to the API.
+     * @private
+     * @type {WebSocketClient}
+     * @memberof Client
+     */
+    private ws: WebSocketClient = new WebSocketClient(this);
+
+    /**
+     *
+     * The token for client.
+     * @private
+     * @type {(string|null)}
+     * @memberof Client
+     */
+    private token: string | null | undefined = null;
+
+    /**
+     * Creates an instance of Client.
+     * @param {ClientOptions} [options]
+     * @memberof Client
+     */
+    public constructor(options?: ClientOptions) {
+        super();
+        this.options = { ...options };
+    }
+
+    /**
+     *
+     * The client logs in.
+     * @memberof Client
+     */
+    public async login(token?: string) {
+        if (
+            this.token === null ||
+            this.token === undefined ||
+            !this.token ||
+            typeof this.token !== "string"
+        ) {
+            this.token = token;
+        }
+
+        if (!this.token) {
+            throw new Error("Token invalid.");
+        }
+        await this.ws.connect();
+    }
+
+    public get getToken() {
+        return this.token;
+    }
+}
