53ceabe806ec70f107e8fd0c9ecc03d9d057c1fa
wioenena-q <brs_kprl09@outlook.com>
2021-06-19 01:19:41 +0300

    Created 'OPCodes' enum.

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -2,3 +2,17 @@ export enum Discord {
     GATEWAY="wss://gateway.discord.gg/?v=9&encoding=json",
     API="https://discord.com/api/v9"
 }
+
+export enum OPCodes {
+    DISPATCH,
+    HEARTBEAT,
+    IDENTIFY,
+    PRESENCE_UPDATE,
+    VOICE_STATE_UPDATE,
+    RESUME,
+    RECONNECT,
+    REQUEST_GUILD_MEMBERS,
+    INVALID_SESSION,
+    HELLO,
+    HEARTBEAT_ACK
+}
