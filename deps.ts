650fe47ef7c5e736e721a63e632d158269259567
wioenena-q <brs_kprl09@outlook.com>
2021-06-19 13:10:21 +0300

    Exported 'Tiger' module from 'deps.ts'

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export { Collection } from "https://deno.land/x/dinordcollection@1.0.2/mod.ts";
 export { EventEmitter } from "https://deno.land/std@0.99.0/node/events.ts";
+export { Tiger } from "https://deno.land/x/tiger@1.0.4/mod.ts";
