952f3defc89a2ce320eefd03ee6ca7f435a0577e
wioenena-q <brs_kprl09@outlook.com>
2021-06-19 14:13:19 +0300

    The main structure has been created.

---
+++ src/Structures/Base.ts
@@ -0,0 +1,40 @@
+import type { Client } from "../Client/Client.ts";
+
+/**
+ *
+ *
+ * @export
+ * @abstract
+ * @class Base
+ * @template T
+ */
+export abstract class Base<T> {
+    /**
+     *
+     *
+     * @private
+     * @type {Client}
+     * @memberof Base
+     */
+    private client: Client
+
+    /**
+     * Creates an instance of Base.
+     * @param {Client} client
+     * @param {T} data
+     * @memberof Base
+     */
+    public constructor(client: Client, data: T) {
+        this.client = client;
+    }
+
+    /**
+     *
+     * The method to patch this class.
+     * @protected
+     * @abstract
+     * @param {T} data
+     * @memberof Base
+     */
+    protected abstract patch(data: T): void;
+}
