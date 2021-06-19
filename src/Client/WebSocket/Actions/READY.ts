558ceb87b309a5455fc7093ade590c1ff519fc6c
wioenena-q <brs_kprl09@outlook.com>
2021-06-19 14:48:55 +0300

    When the client is ready, ClientUser data is saved.

--- src/Client/WebSocket/Actions/READY.ts
+++ src/Client/WebSocket/Actions/READY.ts
@@ -1,6 +1,12 @@
 import type { Client } from "../../Client.ts";
 import type { IPayload } from "../../../Types/IPayload.ts";
+import { ClientUser } from "../../ClientUser.ts";
+import type { IUser } from "../../../Types/StructureTypes.ts";
 
-export default function (client:Client, { d }:IPayload) {
-    console.log(d);
+export default function (client: Client, { d }: IPayload) {
+    client.setUser = new ClientUser(client, (d as Record<string, unknown>).user as IUser);
+
+    client.getWsClient.setGuildSize = (d as any).guilds.length;
+
+    console.log(client);
 }
