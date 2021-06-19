680481d8bea95d65c152152f92655dbb65d4f615
wioenena-q <brs_kprl09@outlook.com>
2021-06-19 22:17:12 +0300

    A method was written to find the creation time from the id.

--- src/Utils/Util.ts
+++ src/Utils/Util.ts
@@ -1,4 +1,5 @@
 import { Tiger } from "../../deps.ts";
+import { Discord } from "./Constants.ts";
 
 /**
  *
@@ -15,4 +16,22 @@ export class Util {
      * @memberof Util
      */
     public static Logger: Tiger = new Tiger();
+
+    /**
+     *
+     * Getting the created timestamp from the id.
+     * @static
+     * @param {(string | number)} id
+     * @returns {number}
+     * @memberof Util
+     */
+    public static idToTimestamp(id: string | number): number {
+        if (typeof id === "string")
+            // eslint-disable-next-line radix
+            id = parseInt(id);
+
+        const binary = id.toString(2).padStart(64, "0").substring(0, 42);
+        const decimal = parseInt(binary, 2);
+        return new Date(decimal + Discord.EPOCH).getTime();
+    }
 }
