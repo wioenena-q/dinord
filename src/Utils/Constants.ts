ed74d565947dfa34665cb1175e0d8afa56116f24
wioenena-q <brs_kprl09@outlook.com>
2021-06-19 22:11:21 +0300

    Added EPOCH to the 'Discord' enum.

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -1,6 +1,7 @@
 export enum Discord {
     GATEWAY = "wss://gateway.discord.gg/?v=9&encoding=json",
-    API = "https://discord.com/api/v9"
+    API = "https://discord.com/api/v9",
+    EPOCH = 1420070400000
 }
 
 export enum OPCodes {
