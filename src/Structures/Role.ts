8620eddf125a5ccaadc6c0f736932b3f73ed9d3a
wioenena-q <brs_kprl09@outlook.com>
2021-06-20 14:17:12 +0300

    A 'Role' structure has been created for guilds.

---
+++ src/Structures/Role.ts
@@ -0,0 +1,48 @@
+import { Base } from "./Base.ts";
+import type { GuildRoleData } from "../Types/StructureTypes.ts";
+import type { Client } from "../Client/Client.ts";
+import type { Snowflake } from "../Types/Snowflake.ts";
+import { Util } from "../Utils/Util.ts";
+
+/**
+ *
+ * The User structure for discord users.
+ * @export
+ * @class User
+ * @extends {Base<GuildRoleData>}
+ */
+export class Role extends Base<GuildRoleData> {
+    private id: Snowflake;
+
+    private name!: string;
+
+    private color!: number;
+
+    private hoist!: boolean;
+
+    private position!: number;
+
+    private permissions!: string;
+
+    private managed!: boolean;
+
+    private mentionable!: boolean;
+
+    public constructor(client: Client, data: GuildRoleData) {
+        super(client);
+        this.id = data.id;
+        this.patch(data);
+    }
+
+    protected patch(data: GuildRoleData): void {
+        this.name = data.name;
+        this.color = data.color;
+        this.hoist = data.hoist;
+        this.position = data.position;
+        this.permissions = data.permissions;
+        this.managed = data.managed || false;
+        this.mentionable = data.mentionable || false;
+    }
+
+    public get getID() { return this.id; }
+}
