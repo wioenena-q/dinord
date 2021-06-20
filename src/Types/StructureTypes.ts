328fc92ededa9a538283f97c426d2fae407fb3de
wioenena-q <brs_kprl09@outlook.com>
2021-06-20 12:16:28 +0300

    An interface has been created for the guild data sent by the API.

--- src/Types/StructureTypes.ts
+++ src/Types/StructureTypes.ts
@@ -17,4 +17,52 @@ export interface GuildData {
     id: Snowflake;
     name: string;
     icon?: string;
+    icon_hash?: string;
+    splash?: string;
+    discovery_splash?: string;
+    owner_id: Snowflake;
+    region?: string;
+    afk_channel_id?: Snowflake;
+    afk_timeout: number;
+    widget_enabled: boolean;
+    widget_channel_id?: Snowflake;
+    verification_level: number;
+    default_message_notifications: number;
+    explicit_content_filter: number;
+    roles: GuildRoleData[];
+    emojis: GuildEmojiData[];
+    features: string[];
+    mfa_level: number;
+    application_id?: Snowflake;
+    system_channel_id?: Snowflake;
+    system_channel_flags: number;
+    rules_channel_id?: Snowflake;
+    joined_at: number;
+    large: boolean;
+    unavailable: boolean;
+    member_count: number;
+    voice_states: GuildVoiceStateData[];
+    members: GuildMemberData[];
+    channels: GuildChannelData[];
+    max_presences?: number;
+    max_members: number;
+    vanity_url_code?: string;
+    description?: string;
+    banner?: string;
+    premium_tier: number;
+    premium_subscription_count: number;
+    preferred_locale: string;
+    max_video_channel_users: number;
+    approximate_member_count: number;
+    approximate_presence_count: number;
+    nsfw_level: number;
 }
+
+export interface GuildRoleData { }
+
+export interface GuildEmojiData { }
+
+export interface GuildVoiceStateData { }
+export interface GuildMemberData { }
+export interface GuildChannelData { }
+export interface GuildPresenceData { }
