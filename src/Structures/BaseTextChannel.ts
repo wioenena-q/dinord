db9b94cf8a7cf492b9cd9a7d947e51b9241f4084
wioenena-q <brs_kprl09@outlook.com>
2021-06-22 23:10:46 +0300

    The main text channel structure was created for the 'DMChannel', 'GroupDMChannel' and 'GuildTextChannel' structures.

---
+++ src/Structures/BaseTextChannel.ts
@@ -0,0 +1,24 @@
+import { Channel } from "./Channel.ts";
+import type { Snowflake } from "../Types/Snowflake.ts";
+import type { BaseTextChannelData } from "../Types/ChannelTypes.ts";
+import type { Client } from "../Client/Client.ts";
+
+/**
+ *
+ * The base text channel structure for (DMChannel | GroupDMChannel | GuildTextChannel) structures.
+ * @export
+ * @class BaseTextChannel
+ * @extends {Channel<BaseTextChannelData>}
+ */
+export class BaseTextChannel extends Channel<BaseTextChannelData> {
+    private lastMessageID?: Snowflake | null;
+    public constructor(client: Client, data: BaseTextChannelData) {
+        super(client, data);
+        this.patch(data);
+    }
+
+    protected patch(data: BaseTextChannelData) {
+        super.patch(data);
+        this.lastMessageID = data.last_message_id;
+    }
+}
