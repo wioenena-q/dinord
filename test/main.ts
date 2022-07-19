2a1f2b701f0c7c3d7d90008adf0d085c3cea4c67
wioenena-q <wioenena@gmail.com>
2022-07-19 04:53:36 +0300

    .

--- test/main.ts
+++ test/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import { Client } from "../src/Client/Client.ts";
+import { ClientConfig } from "../src/Client/ClientConfig.ts";
+import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts?code";
+
+config({ export: true });
+
+const client = new Client(new ClientConfig(Deno.env.get("TOKEN")));
+
+client.login();
