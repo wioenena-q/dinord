8305e37761da7e604b2e4f45ec214be41cac4de3
wioenena-q <wioenena@gmail.com>
2022-07-19 21:20:59 +0300

    is now ws connectable

--- src/Client/Client.ts
+++ src/Client/Client.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from "../deps.ts";
 import { ClientConfig } from "./ClientConfig.ts";
+import { WebSocketClient } from "./WebSocketClient.ts";
 
 export class Client extends EventEmitter {
   #config: ClientConfig;
+  #ws = new WebSocketClient(this);
 
   public constructor();
   public constructor(config: ClientConfig);
@@ -29,10 +31,18 @@ export class Client extends EventEmitter {
       this.#config.token = token;
     }
 
+    if (!this.#ws.socket) {
+      this.#ws.connect();
+    }
+
     return Promise.resolve(void 0);
   }
 
   public get config(): ClientConfig {
     return this.#config;
+  }
+
+  public get ws(): WebSocketClient {
+    return this.#ws;
   }
 }
