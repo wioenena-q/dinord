4a00ee4a1245c05773744f9ae0e6b2c517ada514
wioenena-q <wioenena@gmail.com>
2022-07-19 21:21:34 +0300

    created interface for 'Payload' structure

--- src/Utils/Types.ts
+++ src/Utils/Types.ts
@@ -1,3 +1,10 @@
 export type INullable<T> = T | null | undefined;
 export const isString = (val: unknown): val is string =>
   typeof val === "string";
+
+export interface IPayloads {
+  op: number;
+  d?: Record<string, unknown>;
+  s?: number;
+  t?: string;
+}
