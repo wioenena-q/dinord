a73793562cb12a787dacb654da7d814cf6bbdf94
wioenena-q <wioenena@gmail.com>
2022-07-20 11:38:19 +0300

    IntentFlags added

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -1,5 +1,7 @@
 export const WSS = "wss://gateway.discord.gg/?v=10&encoding=json";
-export enum OPCodes {
+
+// Discord Gateway Opcodes. Ref https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-opcodes
+export const enum OPCodes {
   DISPATCH,
   HEARTBEAT,
   IDENTIFY,
@@ -13,7 +15,8 @@ export enum OPCodes {
   HEARTBEAT_ACK
 }
 
-export enum GatewayCloseEventCodes {
+// Discord Gateway Close Event Codes. Ref https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-close-event-codes
+export const enum GatewayCloseEventCodes {
   UNKNOWN_ERROR = 4000,
   UNKNOWN_OPCODE = 4001,
   DECODE_ERROR = 4002,
@@ -28,4 +31,43 @@ export enum GatewayCloseEventCodes {
   INVALID_API_VERSION = 4012,
   INVALID_INTENTS = 4013,
   DISALLOWED_INTENTS = 4014
+}
+
+// Discord Gateway Intents. Ref https://discord.com/developers/docs/topics/gateway#gateway-intents
+export const enum IntentFlags {
+  GUILDS = 1 << 0,
+  GUILD_MEMBERS = 1 << 1,
+  GUILD_BANS = 1 << 2,
+  GUILD_EMOJIS_AND_STICKERS = 1 << 3,
+  GUILD_INTEGRATIONS = 1 << 4,
+  GUILD_WEBHOOKS = 1 << 5,
+  GUILD_INVITES = 1 << 6,
+  GUILD_VOICE_STATES = 1 << 7,
+  GUILD_PRESENCES = 1 << 8,
+  GUILD_MESSAGES = 1 << 9,
+  GUILD_MESSAGE_REACTIONS = 1 << 10,
+  GUILD_MESSAGE_TYPING = 1 << 11,
+  DIRECT_MESSAGES = 1 << 12,
+  DIRECT_MESSAGE_REACTIONS = 1 << 13,
+  DIRECT_MESSAGE_TYPING = 1 << 14,
+  MESSAGE_CONTENT = 1 << 15,
+  GUILD_SCHEDULED_EVENTS = 1 << 16,
+  AUTO_MODERATION_CONFIGURATION = 1 << 20,
+  AUTO_MODERATION_EXECUTION = 1 << 21,
+  ALL = GUILDS |
+    GUILD_MEMBERS |
+    GUILD_BANS |
+    GUILD_EMOJIS_AND_STICKERS |
+    GUILD_INTEGRATIONS |
+    GUILD_WEBHOOKS |
+    GUILD_INVITES |
+    GUILD_VOICE_STATES |
+    GUILD_PRESENCES |
+    GUILD_MESSAGES |
+    GUILD_MESSAGE_REACTIONS |
+    GUILD_MESSAGE_TYPING |
+    MESSAGE_CONTENT |
+    GUILD_SCHEDULED_EVENTS |
+    AUTO_MODERATION_CONFIGURATION |
+    AUTO_MODERATION_EXECUTION
 }
