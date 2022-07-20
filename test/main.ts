2ee52a4badf3881de65a9997bac395a213e8ed78
wioenena-q <wioenena@gmail.com>
2022-07-20 11:39:33 +0300

    .

--- test/main.ts
+++ test/main.ts
@@ -1,9 +1,17 @@
 import { Client } from "../src/Client/Client.ts";
 import { ClientConfig } from "../src/Client/ClientConfig.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts?code";
+import { IntentFlags } from "../src/Utils/Constants.ts";
 
 config({ export: true });
 
-const client = new Client(new ClientConfig(Deno.env.get("TOKEN")));
+const client = new Client(
+  new ClientConfig(
+    new ClientConfig({
+      token: Deno.env.get("TOKEN"),
+      intents: IntentFlags.ALL
+    })
+  )
+);
 
 client.login();
