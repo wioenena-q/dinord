17efe5a131e5397f81bb3f1abd66a91fdc81a60e
wioenena-q <wioenena@gmail.com>
2022-07-21 05:45:20 +0300

    IUser implemented

--- src/Utils/ApiTypes.ts
+++ src/Utils/ApiTypes.ts
@@ -69,7 +69,23 @@ export interface IInstallParams {
   permissions: string;
 }
 
-export interface IUser {}
+export interface IUser {
+  id: Snowflake;
+  username: string;
+  discriminator: string;
+  avatar?: string;
+  bot?: boolean;
+  system?: boolean;
+  mfa_enabled?: boolean;
+  banner?: string;
+  accent_color?: number;
+  locale?: string;
+  verified?: boolean;
+  email?: string;
+  flags?: number;
+  premium_type?: number;
+  public_flags?: number;
+}
 
 export interface IGuild {
   id: Snowflake;
