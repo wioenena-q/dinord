a57139e3f98b1f35f427f6266696dfb979c7b876
wioenena-q <wioenena@gmail.com>
2022-07-22 10:22:43 +0300

    .

--- src/Utils/dev.ts
+++ src/Utils/dev.ts
@@ -1,8 +1,8 @@
 // The utility functions here are for the development phase.
 
-export const Debug = <T extends { new (...args: any[]): any }>(
-  constructor: T
-) => {
+import { IConstructor } from './Types.ts';
+
+const Debug = (constructor: IConstructor): any => {
   return class extends constructor {
     debug() {
       const o = {
@@ -19,3 +19,30 @@ export const Debug = <T extends { new (...args: any[]): any }>(
     }
   };
 };
+
+function Todo(constructor: IConstructor): void;
+function Todo(target: any, propertyKey: string, descriptor: PropertyDescriptor): void;
+function Todo(target: any, propertyKey: string): void;
+function Todo(...args: unknown[]): void {
+  switch (args.length) {
+    case 1: {
+      const [constructor] = args as [IConstructor];
+      console.log(`class ${constructor.name} is not yet implemented.`);
+      break;
+    }
+    case 2: {
+      const [target, propertyKey] = args as [any, string];
+      console.log(`${target.constructor.name}.${propertyKey} is not yet implemented.`);
+      break;
+    }
+    case 3: {
+      const [target, propertyKey] = args as [any, string, PropertyDescriptor];
+      console.log(
+        `method ${target.constructor.name} -> ${propertyKey} is not yet implemented.`
+      );
+      break;
+    }
+  }
+}
+
+export { Todo, Debug };
