4ffc03eac58a51fe69622074e77cc755ae4cea4a
wioenena-q <wioenena@gmail.com>
2022-07-22 10:17:54 +0300

    #region comments added and few interface implements

--- src/Structures/Base.ts
+++ src/Structures/Base.ts
@@ -1,13 +1,25 @@
 import { Client } from '../Client/Client.ts';
+import { ToJSON, ToString } from '../Utils/Types.ts';
 
-export abstract class Base {
+export abstract class Base implements ToJSON, ToString {
+  // #region Fields
   #client: Client;
+  // #endregion
 
+  // #region Constructor
   public constructor(client: Client) {
     this.#client = client;
   }
+  // #endregion
 
+  // #region Methods
+  public abstract toJSON(): Record<string, unknown>;
+  public abstract toString(): string;
+  // #endregion
+
+  // #region Getter & Setter
   public get client() {
     return this.#client;
   }
+  // #endregion
 }
