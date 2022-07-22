20ebd8d6f8c1656c1e9ac2774c6edd13030d2d81
wioenena-q <wioenena@gmail.com>
2022-07-22 10:17:00 +0300

    #region comments added

--- src/Client/ClientUser.ts
+++ src/Client/ClientUser.ts
@@ -2,4 +2,13 @@ import { User } from '../Structures/User.ts';
 import { Debug } from '../Utils/dev.ts';
 
 @Debug
-export class ClientUser extends User {}
+export class ClientUser extends User {
+  // #region Fields
+  // #endregion
+  // #region Constructor
+  // #endregion
+  // #region Methods
+  // #endregion
+  // #region Getter & Setter
+  // #endregion
+}
