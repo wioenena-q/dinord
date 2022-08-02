cedf13c5e38549fd052ca6a63416611d7a15db94
wioenena-q <wioenena@gmail.com>
2022-08-02 17:53:49 +0300

    Utility implemented and functions tested

--- src/Utils/Utils.ts
+++ src/Utils/Utils.ts
@@ -1,8 +1,39 @@
-import { IConstructor } from './Types.ts';
+export const getTimestampFromId = (id: string): number => {
+  const bId = BigInt(id);
+  return Number((bId >> 22n) + 1420070400000n);
+};
 
-export class Utils extends null {
-  public static getTimestampFromId(id: string): number {
-    const bId = BigInt(id);
-    return Number((bId >> 22n) + 1420070400000n);
-  }
-}
+export const isArray = (val: unknown): val is unknown[] =>
+  isInstanceOf<Array<unknown>>(val, Array) && Array.isArray(val);
+
+export const isString = (val: unknown): val is string => typeof val === 'string';
+
+export const isNumber = (val: unknown): val is number => typeof val === 'number';
+
+export const isObject = <O = Record<string | number | symbol, unknown>>(
+  val: unknown
+): val is O => val !== null && typeof val === 'object';
+
+export const isFunction = (val: unknown): val is (...args: never[]) => unknown =>
+  typeof val === 'function';
+
+export const isBoolean = (val: unknown): val is boolean => typeof val === 'boolean';
+
+export const isInstanceOf = <T>(
+  val: unknown,
+  cls: { new (...args: never[]): unknown }
+): val is T => val instanceof cls;
+
+export const toObject = (target: any, keys: string[]) => {
+  return keys.reduce((acc, key) => {
+    const value = Reflect.get(target, key);
+    if (value !== undefined) {
+      if (isObject(value)) {
+        if (isFunction(value.toObject)) {
+          acc[key] = value.toObject();
+        } else acc[key] = value;
+      } else acc[key] = value;
+    }
+    return acc;
+  }, {} as Record<string | number | symbol, unknown>);
+};
---
+++ test/Utils/Utils.test.ts
@@ -0,0 +1,167 @@
+import {
+  isArray,
+  isBoolean,
+  isFunction,
+  isInstanceOf,
+  isNumber,
+  isObject,
+  isString,
+  toObject
+} from '../../src/Utils/Utils.ts';
+import { assertEquals, assertObjectMatch } from 'https://deno.land/std/testing/asserts.ts';
+
+Deno.test('isArray', () => {
+  assertEquals(isArray([1, 2, 3]), true);
+  assertEquals(isArray(Array.from({ length: 10 })), true);
+  assertEquals(isArray([...[1, 2, 3]]), true);
+  assertEquals(isArray({}), false);
+  assertEquals(isArray(1), false);
+  assertEquals(isArray(true), false);
+  assertEquals(
+    isArray(() => {}),
+    false
+  );
+});
+
+Deno.test('isString', () => {
+  assertEquals(isString(''), true);
+  assertEquals(isString(1), false);
+  assertEquals(isString({}), false);
+  assertEquals(isString(true), false);
+  assertEquals(
+    isString(() => {}),
+    false
+  );
+});
+
+Deno.test('isNumber', () => {
+  assertEquals(isNumber(1), true);
+  assertEquals(isNumber(''), false);
+  assertEquals(isNumber({}), false);
+  assertEquals(isNumber(true), false);
+  assertEquals(
+    isNumber(() => {}),
+    false
+  );
+});
+
+Deno.test('isObject', () => {
+  assertEquals(isObject({}), true);
+  assertEquals(isObject([]), true);
+  assertEquals(isObject(1), false);
+  assertEquals(isObject(''), false);
+  assertEquals(isObject(true), false);
+  assertEquals(
+    isObject(() => {}),
+    false
+  );
+
+  class Class {}
+  assertEquals(isObject(new Class()), true);
+  assertEquals(isObject(Class), false);
+});
+
+Deno.test('isFunction', () => {
+  assertEquals(
+    isFunction(() => {}),
+    true
+  );
+  assertEquals(
+    isFunction(() => 1),
+    true
+  );
+  assertEquals(
+    isFunction(function () {}),
+    true
+  );
+  assertEquals(isFunction(1), false);
+  assertEquals(isFunction(''), false);
+  assertEquals(isFunction({}), false);
+  assertEquals(isFunction(true), false);
+  class Class {}
+  assertEquals(isFunction(Class), true);
+  assertEquals(isFunction(new Class()), false);
+});
+
+Deno.test('isBoolean', () => {
+  assertEquals(isBoolean(true), true);
+  assertEquals(isBoolean(false), true);
+  assertEquals(isBoolean(!!{}), true);
+  assertEquals(isBoolean(1), false);
+  assertEquals(isBoolean(''), false);
+  assertEquals(isBoolean({}), false);
+  assertEquals(
+    isBoolean(() => {}),
+    false
+  );
+});
+
+Deno.test('isInstanceOf', () => {
+  class Class {
+    prop = 10;
+  }
+  assertEquals(isInstanceOf(new Class(), Class), true);
+  assertEquals(isInstanceOf(Class, Class), false);
+  assertEquals(isInstanceOf(1, Class), false);
+  assertEquals(isInstanceOf('', Class), false);
+  assertEquals(isInstanceOf({}, Class), false);
+  assertEquals(isInstanceOf(true, Class), false);
+  assertEquals(
+    isInstanceOf(() => {}, Class),
+    false
+  );
+
+  let v: unknown = {};
+
+  if (!isInstanceOf<Class>(v, Class)) {
+    // @ts-expect-error - not instance of Class
+    v.prop;
+  }
+});
+
+Deno.test('toObject', () => {
+  class First {
+    a = 10;
+    b = [1, 2, 3];
+    c = {
+      a: 1
+    };
+    d = new Map([
+      ['a', 1],
+      ['b', 2],
+      ['c', 3]
+    ]);
+    e = new Set([1, 2, 3]);
+    f = new Second();
+
+    toObject() {
+      return toObject(this, ['a', 'b', 'c', 'd', 'e', 'f']);
+    }
+  }
+
+  class Second {
+    a = 20;
+
+    toObject() {
+      return toObject(this, ['a']);
+    }
+  }
+
+  const obj = new First();
+  assertObjectMatch(obj.toObject(), {
+    a: 10,
+    b: [1, 2, 3],
+    c: {
+      a: 1
+    },
+    d: new Map([
+      ['a', 1],
+      ['b', 2],
+      ['c', 3]
+    ]),
+    e: new Set([1, 2, 3]),
+    f: {
+      a: 20
+    }
+  });
+});
