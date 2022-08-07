a7b8b93b731a7274b943655454303e087a320375
wioenena-q <wioenena@gmail.com>
2022-08-07 18:46:00 +0300

    .

--- src/Utils/dev.ts
+++ src/Utils/dev.ts
@@ -1,48 +1,24 @@
 // The utility functions here are for the development phase.
 
-import { IConstructor } from './Types.ts';
+import { IntentFlags } from 'https://raw.githubusercontent.com/wioenena-q/dinord-api-types/master/src/api/v10/Intents.ts';
 
-const Debug = (constructor: IConstructor): any => {
-  return class extends constructor {
-    debug() {
-      const o = {
-        [constructor.name]: {}
-      } as any;
-
-      const keys = Reflect.ownKeys(constructor.prototype);
-      for (const key of keys) {
-        if (key === 'constructor') continue;
-        const value = Reflect.get(this, key);
-        o[constructor.name][key] = value;
-      }
-      console.log('%s %O', constructor.name, o[constructor.name]);
-    }
-  };
-};
-
-function Todo(constructor: IConstructor): void;
-function Todo(target: any, propertyKey: string, descriptor: PropertyDescriptor): void;
-function Todo(target: any, propertyKey: string): void;
-function Todo(...args: unknown[]): void {
-  switch (args.length) {
-    case 1: {
-      const [constructor] = args as [IConstructor];
-      console.log(`class ${constructor.name} is not yet implemented.`);
-      break;
-    }
-    case 2: {
-      const [target, propertyKey] = args as [any, string];
-      console.log(`${target.constructor.name}.${propertyKey} is not yet implemented.`);
-      break;
-    }
-    case 3: {
-      const [target, propertyKey] = args as [any, string, PropertyDescriptor];
-      console.log(
-        `method ${target.constructor.name} -> ${propertyKey} is not yet implemented.`
-      );
-      break;
-    }
-  }
+export function allIntents() {
+  return [
+    IntentFlags.Guilds,
+    IntentFlags.GuildMembers,
+    IntentFlags.GuildBans,
+    IntentFlags.GuildEmojisAndStickers,
+    IntentFlags.GuildIntegrations,
+    IntentFlags.GuildWebhooks,
+    IntentFlags.GuildInvites,
+    IntentFlags.GuildMessageReactions,
+    IntentFlags.GuildMessageTyping,
+    IntentFlags.DirectMessages,
+    IntentFlags.DirectMessageReactions,
+    IntentFlags.DirectMessageTyping,
+    IntentFlags.MessageContent,
+    IntentFlags.GuildScheduledEvents,
+    IntentFlags.AutoModerationConfiguration,
+    IntentFlags.AutoModerationExecution
+  ].reduce((p, c) => p + c);
 }
-
-export { Todo, Debug };
