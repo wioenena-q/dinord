bc72803f02ff71725b99bcffe996e395deabab4c
wioenena-q <wioenena@gmail.com>
2022-08-12 00:39:21 +0300

    deleted files

--- src/Client/Events/READY.ts
+++
@@ -1,8 +0,0 @@
-import type { Client } from '../Client.ts';
-import type { IReadyPayloadData } from '../../Utils/ApiTypes.ts';
-import { ClientUser } from '../ClientUser.ts';
-
-export default (client: Client, d: IReadyPayloadData) => {
-  client.ws.guildSize = d.guilds.length;
-  client.user = new ClientUser(client, d.user);
-};
