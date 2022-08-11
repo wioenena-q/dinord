040e7a9b159c4b97aa9bcdf61da373d6ab11f8df
wioenena-q <wioenena@gmail.com>
2022-08-12 00:38:11 +0300

    deleted files

--- src/Structures/Channel/TextBasedChannel.ts
+++
@@ -1,6 +0,0 @@
-import { Snowflake } from '../../Utils/ApiTypes.ts';
-import { INullable } from '../../Utils/Types.ts';
-
-export interface ITextBasedChannel {
-  lastMessageId: Snowflake | INullable<Snowflake>;
-}
--- src/Structures/Guild/Channel/GuildTextBasedChannel.ts
+++
@@ -1,5 +0,0 @@
-import { ITextBasedChannel } from '../../Channel/TextBasedChannel.ts';
-
-export interface IGuildTextBasedChannel extends ITextBasedChannel {
-  rateLimitPerUser: number;
-}
