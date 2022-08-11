6bb33557b011e492cdb9aeb057d98235f8e7a9e5
wioenena-q <wioenena@gmail.com>
2022-08-12 00:37:59 +0300

    implemented 'clone', 'update' methods

--- src/Structures/Base.ts
+++ src/Structures/Base.ts
@@ -1,25 +1,37 @@
-import { Client } from '../Client/Client.ts';
-import { ToJSON, ToString } from '../Utils/Types.ts';
+import type { Client } from '../Client/Client.ts';
+import type { Clonable, Patchable, ToJSON, ToString, Updatable } from '../Utils/Types.ts';
 
-export abstract class Base implements ToJSON, ToString {
-  // #region Fields
+export abstract class Base implements ToJSON, ToString, Patchable, Clonable, Updatable {
   #client: Client;
-  // #endregion
 
-  // #region Constructor
   public constructor(client: Client) {
     this.#client = client;
   }
-  // #endregion
 
-  // #region Methods
+  /**
+   * Clone this object
+   * @returns {this}
+   */
+  public clone() {
+    return Object.assign(Object.create(this), this);
+  }
+
+  /**
+   * Update this object and return old object
+   * @param data - Data to update
+   * @returns {this}
+   */
+  public update(data: unknown) {
+    const clone = this.clone();
+    this.patch(data);
+    return clone;
+  }
+
   public abstract toJSON(): Record<string, unknown>;
   public abstract toString(): string;
-  // #endregion
+  public abstract patch(data: unknown): void;
 
-  // #region Getter & Setter
   public get client() {
     return this.#client;
   }
-  // #endregion
 }
