3c34950f9784a588edafcbea12ffe75c799e04ea
wioenena-q <wioenena@gmail.com>
2022-08-12 16:29:51 +0300

    .

--- test/main.ts
+++ test/main.ts
@@ -11,6 +11,22 @@ const client = new Client({
   }
 });
 
+client.on('ready', () => {
+  console.log('ready');
+});
+
+client.on('guildCreate', (guild) => {
+  console.log(`Client joined guild: ${guild.name}`);
+});
+
+client.on('guildDelete', (guild) => {
+  console.log(`Client left guild: ${guild.name}`);
+});
+
+client.on('guildUpdate', (oldGuild, newGuild) => {
+  console.log(`Client guild update: ${oldGuild.name} => ${newGuild.name}`);
+});
+
 client.on('shardReady', (shard) => {
   console.log(shard.toString());
 });
