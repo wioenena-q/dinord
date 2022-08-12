dd115dd54a5ed135c5192cdbbc99e3d3ab75c535
wioenena-q <wioenena@gmail.com>
2022-08-12 09:25:57 +0300

    reimplemented types to isInstanceOf

--- src/Utils/Utils.ts
+++ src/Utils/Utils.ts
@@ -3,8 +3,7 @@ export const getTimestampFromId = (id: string): number => {
   return Number((bId >> 22n) + 1420070400000n);
 };
 
-export const isArray = (val: unknown): val is unknown[] =>
-  isInstanceOf<Array<unknown>>(val, Array) && Array.isArray(val);
+export const isArray = (val: unknown): val is unknown[] => isInstanceOf(val, Array) && Array.isArray(val);
 
 export const isString = (val: unknown): val is string => typeof val === 'string';
 
@@ -13,12 +12,14 @@ export const isNumber = (val: unknown): val is number => typeof val === 'number'
 export const isObject = <O = Record<PropertyKey, unknown>>(val: unknown): val is O =>
   val !== null && typeof val === 'object';
 
-export const isFunction = (val: unknown): val is (...args: never[]) => unknown => typeof val === 'function';
+export const isFunction = (val: unknown): val is (...args: unknown[]) => unknown => typeof val === 'function';
 
 export const isBoolean = (val: unknown): val is boolean => typeof val === 'boolean';
 
-export const isInstanceOf = <T>(val: unknown, cls: { new (...args: never[]): unknown }): val is T =>
-  val instanceof cls;
+export const isInstanceOf = <T extends { new (...args: never[]): unknown }>(
+  val: unknown,
+  cls: T
+): val is InstanceType<T> => val instanceof cls;
 
 export const toObject = (target: any, keys: string[]): Record<PropertyKey, unknown> => {
   // https://stackoverflow.com/questions/33605775/es6-dynamic-class-names
