7bb1824f4392b29c413c2ff01c3520f8a7fd7e34
wioenena-q <wioenena@gmail.com>
2022-08-12 16:26:42 +0300

    some event added

--- src/Client/ws/Events/EventManager.ts
+++ src/Client/ws/Events/EventManager.ts
@@ -2,6 +2,9 @@ import { GatewayDispatchEvents } from 'https://deno.land/x/discord_api_types@0.3
 import { Collection } from '../../../deps.ts';
 import { WebSocketManager } from '../WebSocketManager.ts';
 import type { Base } from './Base.ts';
+import { GuildCreateEvent } from './GUILD_CREATE.ts';
+import { GuildDeleteEvent } from './GUILD_DELETE.ts';
+import { GuildUpdateEvent } from './GUILD_UPDATE.ts';
 import { ReadyEvent } from './READY.ts';
 
 /**
@@ -24,6 +27,15 @@ export class EventManager extends Collection<GatewayDispatchEvents, Base> {
       switch (eventName) {
         case GatewayDispatchEvents.Ready:
           this.set(GatewayDispatchEvents.Ready, new ReadyEvent(this.#ws.client));
+          break;
+        case GatewayDispatchEvents.GuildCreate:
+          this.set(GatewayDispatchEvents.GuildCreate, new GuildCreateEvent(this.#ws.client));
+          break;
+        case GatewayDispatchEvents.GuildDelete:
+          this.set(GatewayDispatchEvents.GuildDelete, new GuildDeleteEvent(this.#ws.client));
+          break;
+        case GatewayDispatchEvents.GuildUpdate:
+          this.set(GatewayDispatchEvents.GuildUpdate, new GuildUpdateEvent(this.#ws.client));
           break;
       }
     }
