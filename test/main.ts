7f8cec1a2e3aebd1f0ecc13d919fc75a9710d8b0
wioenena-q <wioenena@gmail.com>
2022-08-13 21:14:21 +0300

    .

--- test/main.ts
+++ test/main.ts
@@ -1,12 +1,12 @@
-import { GatewayIntentBits } from 'https://deno.land/x/discord_api_types@0.37.2/v10.ts';
 import { config } from 'https://deno.land/x/dotenv@v3.2.0/mod.ts?code';
 import { Client } from '../src/Client/Client.ts';
+import { allIntents } from '../src/Utils/dev.ts';
 
 config({ export: true });
 
 const client = new Client({
   ws: {
-    intents: GatewayIntentBits.Guilds,
+    intents: allIntents(),
     shardCount: 1
   }
 });
@@ -27,10 +27,6 @@ client.on('guildUpdate', (oldGuild, newGuild) => {
   console.log(`Client guild update: ${oldGuild.name} => ${newGuild.name}`);
 });
 
-client.on('shardReady', (shard) => {
-  console.log(shard.toString());
-});
+client.on('debug', console.log);
 
-client.login(Deno.env.get('TOKEN')).then(() => {
-  console.log(client.ws.toString());
-});
+client.login(Deno.env.get('TOKEN'));
