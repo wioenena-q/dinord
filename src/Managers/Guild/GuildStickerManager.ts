7d49ce7363e7090ae8a8790ec222733e2e38e001
wioenena-q <wioenena@gmail.com>
2022-08-13 21:12:32 +0300

    GuildStickerManager implemented

--- src/Managers/Guild/GuildStickerManager.ts
+++ src/Managers/Guild/GuildStickerManager.ts
@@ -1,10 +1,18 @@
+import { GuildSticker } from '../../Structures/Guild/GuildSticker.ts';
 import { BaseManagerForGuild } from './BaseManagerForGuildManagers.ts';
 
 import type { Snowflake } from 'https://deno.land/x/discord_api_types@0.37.2/globals.ts';
+import type { APISticker } from 'https://deno.land/x/discord_api_types@0.37.2/v10.ts';
 
 /**
  * @class
  * @classdesc Manager of guild stickers.
- * @extends {BaseManagerForGuild<Snowflake, unknown>}
+ * @extends {BaseManagerForGuild<Snowflake, GuildSticker>}
  */
-export class GuildStickerManager extends BaseManagerForGuild<Snowflake, unknown> {}
+export class GuildStickerManager extends BaseManagerForGuild<Snowflake, GuildSticker> {
+  public add(data: APISticker) {
+    const exists = this.get(data.id);
+    if (exists) exists.patch(data);
+    else this.set(data.id, new GuildSticker(this.guild, data));
+  }
+}
