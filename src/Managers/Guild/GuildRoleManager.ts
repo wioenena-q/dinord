5b08471535cd125e2c52d930a2184fde1c09ea84
wioenena-q <wioenena@gmail.com>
2022-08-13 21:12:08 +0300

    GuildRoleManager implemented

--- src/Managers/Guild/GuildRoleManager.ts
+++ src/Managers/Guild/GuildRoleManager.ts
@@ -1,10 +1,19 @@
+import { Role } from '../../Structures/Guild/Role.ts';
 import { BaseManagerForGuild } from './BaseManagerForGuildManagers.ts';
 
 import type { Snowflake } from 'https://deno.land/x/discord_api_types@0.37.2/globals.ts';
+import { APIRole } from 'https://deno.land/x/discord_api_types@0.37.2/v10.ts';
 
 /**
  * @class
  * @classdesc Manager of guild roles.
- * @extends {BaseManagerForGuild<Snowflake, unknown>}
+ * @extends {BaseManagerForGuild<Snowflake, Role>}
  */
-export class GuildRoleManager extends BaseManagerForGuild<Snowflake, unknown> {}
+export class GuildRoleManager extends BaseManagerForGuild<Snowflake, Role> {
+  public add(data: APIRole) {
+    const exists = this.get(data.id);
+
+    if (exists) exists.patch(data);
+    else this.set(data.id, new Role(this.guild, data));
+  }
+}
