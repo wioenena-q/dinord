5ff2e56c6ae396903647bef0cf30ca5b5a34eb7d
wioenena-q <wioenena@gmail.com>
2022-08-13 21:11:46 +0300

    GuildEmojiManager implemented

--- src/Managers/Guild/GuildEmojiManager.ts
+++ src/Managers/Guild/GuildEmojiManager.ts
@@ -1,10 +1,19 @@
+import { GuildEmoji } from '../../Structures/Guild/GuildEmoji.ts';
 import { BaseManagerForGuild } from './BaseManagerForGuildManagers.ts';
 
 import type { Snowflake } from 'https://deno.land/x/discord_api_types@0.37.2/globals.ts';
+import type { APIEmoji } from 'https://deno.land/x/discord_api_types@0.37.2/v10.ts';
 
 /**
  * @class
  * @classdesc Manager of guild emojis.
- * @extends {BaseManagerForGuild<Snowflake, unknown>}
+ * @extends {BaseManagerForGuild<Snowflake, GuildEmoji>}
  */
-export class GuildEmojiManager extends BaseManagerForGuild<Snowflake, unknown> {}
+export class GuildEmojiManager extends BaseManagerForGuild<Snowflake, GuildEmoji> {
+  public add(data: APIEmoji) {
+    const exists = this.get(data.id!);
+
+    if (exists) exists.patch(data);
+    else this.set(data.id!, new GuildEmoji(this.guild, data));
+  }
+}
