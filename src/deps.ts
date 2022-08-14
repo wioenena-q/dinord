8d44d43dbdb074d95ef37dee2ff3a04d5d0f9780
wioenena-q <wioenena@gmail.com>
2022-08-14 16:16:38 +0300

    discord api types exported

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
 export { Collection } from 'https://deno.land/x/dinordcollection@1.0.2/mod.ts';
+export * from 'https://deno.land/x/discord_api_types@0.37.2/v10.ts';
 export { EventEmitter } from 'https://deno.land/x/event@2.0.1/mod.ts?code';
