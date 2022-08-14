81f4c070ae99e9aba83b5d9f2203947121ea37ce
wioenena-q <wioenena@gmail.com>
2022-08-14 19:26:31 +0300

    using node events

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
+export { EventEmitter } from 'https://deno.land/std@0.152.0/node/events.ts';
 export { Collection } from 'https://deno.land/x/dinordcollection@1.0.2/mod.ts';
 export * from 'https://deno.land/x/discord_api_types@0.37.2/v10.ts';
-export { EventEmitter } from 'https://deno.land/x/event@2.0.1/mod.ts?code';
