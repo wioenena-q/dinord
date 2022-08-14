932b8fa98e43daf70c152fb60cea3bbdfd5735e6
wioenena-q <wioenena@gmail.com>
2022-08-14 22:37:04 +0300

    Color.toHex and Color.toRGB tested

--- test/Structures/Color.test.ts
+++ test/Structures/Color.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals, assertThrows } from 'https://deno.land/std/testing/asserts.ts';
+import { assert, assertEquals, assertObjectMatch, assertThrows } from 'https://deno.land/std/testing/asserts.ts';
 import { Color } from '../../src/Structures/Color.ts';
 
 Deno.test('Color.resolve() with DefaultColors', () => {
@@ -28,9 +28,27 @@ Deno.test('Color.resolve() with not valid color', () => {
 
   assertThrows(
     () => {
+      // @ts-expect-error - color is not a valid color
       Color.resolve('a'.repeat(100));
     },
     Error,
     `Unknown color: ${'a'.repeat(100)}`
   );
+});
+
+Deno.test('Color.random()', () => {
+  const random = Color.random();
+  assert(random >= 0 && random <= 0xffffff);
+});
+
+Deno.test('Color.toHex()', () => {
+  assertEquals(Color.toHex(Color.resolve('AntiqueWhite')).toUpperCase(), '#FAEBD7'); // with DefaultColors
+  assertEquals(Color.toHex(0xffffff).toUpperCase(), '#FFFFFF'); // with number value
+  assertEquals(Color.toHex(Color.resolve({ r: 255, g: 255, b: 255 })).toUpperCase(), '#FFFFFF'); // with rgb object
+});
+
+Deno.test('Color.toRGB()', () => {
+  assertObjectMatch(Color.toRGB(Color.resolve('AntiqueWhite')), { r: 250, g: 235, b: 215 }); // with DefaultColors
+  assertObjectMatch(Color.toRGB(0xffffff), { r: 255, g: 255, b: 255 }); // with number value
+  assertObjectMatch(Color.toRGB(Color.resolve('#fff')), { r: 255, g: 255, b: 255 }); // with rgb object
 });
