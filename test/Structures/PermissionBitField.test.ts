5807edbf3320d1fa574d80b15b7350fa2a8cd955
wioenena-q <wioenena@gmail.com>
2022-08-15 22:20:01 +0300

    removed console.log and toArray method tested

--- test/Structures/PermissionBitField.test.ts
+++ test/Structures/PermissionBitField.test.ts
@@ -7,8 +7,14 @@ Deno.test('PermissionBitField.resolve() with flags', () => {
 
 Deno.test('PermissionBitField.resolve() invalid bits', () => {
   assertThrows(() => {
-    console.log(new PermissionBitField());
     // @ts-expect-error - invalid bitfield
     PermissionBitField.resolve(0x1);
   }, TypeError);
+});
+
+Deno.test('PermissionBitField.toArray()', () => {
+  const bits = new PermissionBitField();
+  bits.add(PermissionBitField.FLAGS.AddReactions);
+  bits.add(PermissionBitField.FLAGS.ManageMessages);
+  assertEquals(bits.toArray(), ['AddReactions', 'ManageMessages']);
 });
