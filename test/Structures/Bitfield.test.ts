5570f7f8fd394afa5454ad7b4f87576274bff038
wioenena-q <wioenena@gmail.com>
2022-08-15 22:18:04 +0300

    BitField.resolve() tested

--- test/Structures/Bitfield.test.ts
+++ test/Structures/Bitfield.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+import { assert, assertEquals, assertThrows } from 'https://deno.land/std/testing/asserts.ts';
 import { BitField } from '../../src/Structures/BitField.ts';
 
 Deno.test('BitField.defaultBits', () => {
@@ -30,11 +30,40 @@ Deno.test('BitField.remove()', () => {
   assertEquals(bitfield.bits, 0x1 | 0x2 | 0x4 | 0x8);
   bitfield.remove(0x4, 0x8);
   assertEquals(bitfield.bits, 0x1 | 0x2);
-  assertEquals(bitfield.has(0x4), false);
-  assertEquals(bitfield.has(0x8), false);
+  assert(!bitfield.has(0x4));
+  assert(!bitfield.has(0x8));
 });
 
 Deno.test('BitField.missing()', () => {
   const bits = new BitField<number>(0x1, 0x2, 0x4, 0x8);
-  assertEquals(bits.missing(0x1, 0x2, 0x4, 0x8, 0x10), 0x10);
+  assertEquals(bits.missing(0x1, 0x2, 0x4, 0x8, 0x10).bits, 0x10);
+});
+
+Deno.test('BitField.resolve()', () => {
+  assertEquals(BitField.resolve(0x1), 0x1);
+  assertEquals(BitField.resolve([0x1, 0x2, 0x4, 0x8]), 0xf);
+  assertEquals(BitField.resolve(new BitField(0x1)), 0x1);
+});
+
+Deno.test('BitField.eq()', () => {
+  const bits = new BitField(0x1, 0x2, 0x4, 0x8);
+  assert(bits.eq(new BitField(0x1, 0x2, 0x4, 0x8)));
+});
+
+Deno.test('BitField.resolve() invalid bits', () => {
+  assertThrows(() => {
+    BitField.resolve(1n);
+  }, TypeError);
+
+  assertThrows(() => {
+    BitField.resolve(null);
+  }, TypeError);
+
+  assertThrows(() => {
+    BitField.resolve('');
+  }, TypeError);
+
+  assertThrows(() => {
+    BitField.resolve([null]);
+  }, TypeError);
 });
