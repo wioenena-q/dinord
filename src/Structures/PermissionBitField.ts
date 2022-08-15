db7cd6c93cee54b521c7425ee5fcdadaffd3b43d
wioenena-q <wioenena@gmail.com>
2022-08-15 22:13:09 +0300

    flags set

--- src/Structures/PermissionBitField.ts
+++ src/Structures/PermissionBitField.ts
@@ -1,10 +1,31 @@
-import { BitField } from './BitField.ts';
+import { PermissionFlagsBits } from '../deps.ts';
+import { BitField, BitFieldResolvable } from './BitField.ts';
+
+export interface PermissionBitField extends BitField<bigint> {
+  add(...bits: PermissionBitFieldResolvable[]): this;
+  remove(...bits: PermissionBitFieldResolvable[]): this;
+  has(...bits: PermissionBitFieldResolvable[]): boolean;
+  missing(...bits: PermissionBitFieldResolvable[]): PermissionBitField;
+  eq(other: PermissionBitField): boolean;
+  toArray(): PermissionBitFieldFlags[];
+}
 
 /**
  * @class
  * @classdesc Permission BitField for Discord permissions.
- * @extends {BitField<bigint>}
  */
 export class PermissionBitField extends BitField<bigint> {
-  public static readonly defaultBits = 0n;
+  public static readonly FLAGS = PermissionFlagsBits;
+  public static readonly defaultBits: bigint = 0n;
+
+  public constructor(...bits: PermissionBitFieldResolvable[]) {
+    super(...bits);
+  }
+
+  public static resolve(bits: PermissionBitFieldResolvable): bigint {
+    return super.resolve(bits) as bigint;
+  }
 }
+
+export type PermissionBitFieldResolvable = BitFieldResolvable<PermissionBitFieldFlags, bigint>;
+export type PermissionBitFieldFlags = keyof typeof PermissionBitField.FLAGS;
