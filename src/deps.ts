87c1385eacfa5bddf0b66ff13661a2f46279c044
wioenena- <wioenena@gmail.com>
2023-02-05 20:47:26 +0300

    reset

--- src/deps.ts
+++
@@ -1,3 +0,0 @@
-export { EventEmitter } from 'https://deno.land/std@0.152.0/node/events.ts';
-export { Collection } from 'https://deno.land/x/dinordcollection@1.0.2/mod.ts';
-export * from 'https://deno.land/x/discord_api_types@0.37.2/v10.ts';
