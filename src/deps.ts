2bf1819cdb31884f12d23fcb4f21afda3fbac0e9
wioenena- <wioenena@gmail.com>
2023-02-06 01:54:21 +0300

    chore: exported assert

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,2 @@
-export {EventEmitter} from "https://deno.land/std/node/events.ts"
+export { EventEmitter } from 'https://deno.land/std/node/events.ts';
+export { assert } from 'https://deno.land/std/testing/asserts.ts';
