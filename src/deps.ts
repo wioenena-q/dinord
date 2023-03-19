353a711ddf6e24ebf21fbccc94c01b0af2ef031a
wioenena-q <wioenena@gmail.com>
2023-03-19 19:32:14 +0300

    chore: using import map

--- src/deps.ts
+++
@@ -1,4 +0,0 @@
-export { EventEmitter } from 'node:events';
-export { assert } from 'https://deno.land/std@0.178.0/testing/asserts.ts';
-export { constants as ZLIB_CONSTANTS, inflate } from 'node:zlib';
-export { type Buffer } from 'node:buffer';
